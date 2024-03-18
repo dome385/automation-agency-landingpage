4f6a8d7b0b269a3a93382d266b15f7a35d1b94c7
dome385 <dominik.niestroj@gmail.com>
2024-03-18 16:23:25 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-
-import './App.css'
-
 function App() {
-
-
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>
-        Hello World!
-      </h1>
+      <h1 className="text-3xl font-bold underline">Hello World!</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
