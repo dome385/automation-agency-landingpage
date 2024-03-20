cfd9243256ca1d738c9b56950d33cd40c2185fc8
dome385 <dominik.niestroj@gmail.com>
2024-03-20 08:12:14 +0100

    finish

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,31 @@
+import { useLocation } from "react-router-dom";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+
 import { brainwave } from "../assets";
 import { navigation } from "../constants";
-import { useLocation } from "react-router-dom";
-import Button from "./Button.jsx";
-import MenuSvg from "../assets/svg/MenuSvg.jsx";
-import { HamburgerMenu } from "./design/Header.jsx";
+import Button from "./Button";
+import MenuSvg from "../assets/svg/MenuSvg";
+import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
 
 const Header = () => {
-  const pathName = useLocation();
+  const pathname = useLocation();
   const [openNavigation, setOpenNavigation] = useState(false);
 
   const toggleNavigation = () => {
     if (openNavigation) {
       setOpenNavigation(false);
+      enablePageScroll();
     } else {
       setOpenNavigation(true);
+      disablePageScroll();
     }
   };
 
   const handleClick = () => {
+    if (!openNavigation) return;
+
+    enablePageScroll();
     setOpenNavigation(false);
   };
 
@@ -41,31 +48,33 @@ const Header = () => {
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
             {navigation.map((item) => (
               <a
+                key={item.id}
+                href={item.url}
+                onClick={handleClick}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-                  item.url === pathName.hash
+                  item.url === pathname.hash
                     ? "z-2 lg:text-n-1"
                     : "lg:text-n-1/50"
                 } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
-                key={item.id}
-                href={item.url}
               >
                 {item.title}
               </a>
             ))}
-
-            <HamburgerMenu />
           </div>
+
+          <HamburgerMenu />
         </nav>
+
         <a
           href="#signup"
           className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
         >
-          über uns
+          New account
         </a>
         <Button className="hidden lg:flex" href="#login">
-          Kontakt
+          Sign in
         </Button>
 
         <Button
