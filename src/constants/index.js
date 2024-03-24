503a20f236ecf6dbb277224468703afc25b7ceff
dome385 <dominik.niestroj@stud.hshl.de>
2024-03-24 17:29:01 +0100

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -94,21 +94,21 @@ export const brainwaveServicesIcons = [
 export const roadmap = [
   {
     id: "0",
-    title: "Voice recognition",
+    title: "AI Sales Bot um Leads anzurufen",
     text: "Enable the chatbot to understand and respond to voice commands, making it easier for users to interact with the app hands-free.",
     date: "May 2023",
     status: "done",
     imageUrl: roadmap1,
     colorful: true,
   },
-  {
+  /*  {
     id: "1",
     title: "Gamification",
     text: "Add game-like elements, such as badges or leaderboards, to incentivize users to engage with the chatbot more frequently.",
     date: "May 2023",
     status: "progress",
     imageUrl: roadmap2,
-  },
+  }, */
   {
     id: "2",
     title: "Bildungs-AI-Chatbot",
@@ -119,8 +119,8 @@ export const roadmap = [
   },
   {
     id: "3",
-    title: "Integration with APIs",
-    text: "Allow the chatbot to access external data sources, such as weather APIs or news APIs, to provide more relevant recommendations.",
+    title: "API Integration",
+    text: "Ermöglichen Sie dem Chatbot den Zugriff auf externe Datenquelle um auf Dokumente zuzugreifen oder Automationen zu triggern.",
     date: "May 2023",
     status: "progress",
     imageUrl: roadmap4,
