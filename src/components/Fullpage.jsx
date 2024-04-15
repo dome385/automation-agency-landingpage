f8848bb4ba4392b824409fc57fc0de3056087dbd
dome385 <dominik.niestroj@stud.hshl.de>
2024-04-15 20:47:09 +0200

    cookies

--- src/components/Fullpage.jsx
+++ src/components/Fullpage.jsx
@@ -9,10 +9,25 @@ import Roadmap from "../components/Roadmap";
 import Services from "../components/Services";
 import { CookieModal } from "@schlomoh/react-cookieconsent";
 
-const cookieContent = () => (
+const Content = () => (
   <>
-    <h3>Test Test</h3>
-    <p>Bla Bla Bla</p>
+    <h3>Wir verwenden Cookies.</h3>
+    <p>
+      Wir verwenden verschiedene Cookies, um Ihre Website-Erfahrung zu
+      verbessern und Ihre Interaktionen mit unserer Website zu verfolgen. Hier
+      sind einige Beispiele für Cookies, die wir verwenden: {<br />} Technische
+      Cookies: Diese Cookies sind notwendig, um Ihnen die Website zu ermöglichen
+      und sie zu verwenden. {<br />} Sie speichern Informationen wie Ihre
+      Sprache und Ihre Einstellungen. {<br />} Funktionscookies: Diese Cookies
+      helfen uns, Ihre Website-Erfahrung zu verbessern, indem sie Informationen
+      über Ihre Interaktionen mit unserer Website speichern. Sie können
+      beispielsweise helfen, Ihre Einstellungen zu speichern oder Ihre
+      Recherchen zu speichern.{<br />} Marketingcookies: Diese Cookies helfen
+      uns, Ihre Interaktionen mit unserer Website zu verfolgen, um Ihnen
+      persönlich angepasste Inhalte und Werbung anzuzeigen. Sie speichern
+      Informationen über Ihre Interaktionen mit unserer Website und Ihre
+      Vorlieben.
+    </p>
   </>
 );
 
@@ -20,7 +35,11 @@ const Fullpage = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        <CookieModal />
+        <CookieModal
+          infoContent={<Content />}
+          acceptButtonText="Akzeptieren"
+          declineButtonText="Ablehnen"
+        />
         <Header />
         <Hero />
         <Benefits />
