f32d36dc31ad940bf70f90d6edbf171e9ae1ab93
dome385 <dominik.niestroj@stud.hshl.de>
2024-04-29 17:09:47 +0200

    update

--- src/components/CompanyLogos.jsx
+++ src/components/CompanyLogos.jsx
@@ -1,4 +1,4 @@
-import { companyLogos } from "../constants";
+//import { companyLogos } from "../constants";
 
 const CompanyLogos = ({ className }) => {
   return (
@@ -9,7 +9,7 @@ const CompanyLogos = ({ className }) => {
         steigern.&quot; <br />
       </h5>
       *
-      <ul className="flex">
+      {/*  <ul className="flex">
         {companyLogos.map((logo, index) => (
           <li
             className="flex items-center justify-center flex-1 h-[8.5rem]"
@@ -18,7 +18,7 @@ const CompanyLogos = ({ className }) => {
             <img src={logo} width={134} height={28} alt={logo} />
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   );
 };
